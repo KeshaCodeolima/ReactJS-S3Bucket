82a0d7c2b47de48d27cd0bdf6fca2b98db75f2d3
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2025-02-12 14:17:41 +0530

    feat(frontend): Add the main page code with using the div tag and other html elements to design the page.

--- src/Components/mainpage.jsx
+++ src/Components/mainpage.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import './mainpage.css';
 
-function mainpage() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Mainpage() {
+    return (
+        <div className="main">
+            <div className="text">
+                <h1>AWS S3 Bucket.</h1>
+                <h2>Store the Files in the AWS S3 Bucket Using the Javacript.</h2>
+            </div>
+
+            <div className="upload">
+                <h2>Upload Files.</h2>
+                <input type="file" id='fileInput' />
+                <button id='uploadButton' className='btn'>Upload File</button>
+            </div>
+
+            <button className='btn1'>View S3 Bucket List</button>
+
+            <div className="file-list">
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Size</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    )
 }
 
-export default mainpage
+export default Mainpage
