d4f7c437eb767ca74410c934201dd4519d6a750f
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2025-02-12 14:14:06 +0530

    feat(frontend): Add the mainpage to App.js file to display the page using the react router dom.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Mainpage from "./Components/mainpage";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Mainpage />} />
+      </Routes>
     </>
   );
 }
