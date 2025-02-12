d87314d4fb3d9f18a6498f715b77850adb782943
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2025-02-12 16:51:12 +0530

    feat(frontend): Add the useState to set the visibility if click on the button of the View S3 Bucket list

--- src/Components/mainpage.jsx
+++ src/Components/mainpage.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './mainpage.css';
 
 function Mainpage() {
+
+    const [showTable, setShowTable] = useState(false);
+
+    const toshowTable = () => {
+        setShowTable(prevState => !prevState);
+    };
+
     return (
         <div className="main">
             <div className="text">
@@ -15,19 +22,21 @@ function Mainpage() {
                 <button id='uploadButton' className='btn'>Upload File</button>
             </div>
 
-            <button className='btn1'>View S3 Bucket List</button>
+            <button className='btn1' onClick={toshowTable}>{showTable ? 'Hide S3 Bucket List' : 'View S3 Bucket List'}</button>
 
-            <div className="file-list">
+            {showTable && (<div className="file-list">
                 <table>
                     <tr>
                         <th>Name</th>
                         <th>Size</th>
                     </tr>
                     <tr>
-                        <td></td>
+                        <td>image</td>
+                        <td>250.6 KB</td>
                     </tr>
                 </table>
             </div>
+            )}
         </div>
     )
 }
